13c4219fc8426c3117a344b333960788aa9d4e89
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2022-07-05 08:24:50 -0300

    feat: finishing post page content

--- src/pages/post/[slug].tsx
+++ src/pages/post/[slug].tsx
@@ -1,6 +1,7 @@
 import { format } from 'date-fns';
 import { GetStaticPaths, GetStaticProps } from 'next';
-import { RichText } from 'prismic-dom';
+import PrismicDOM from 'prismic-dom';
+import { Fragment } from 'react';
 import Header from '../../components/Header';
 
 import { getPrismicClient } from '../../services/prismic';
@@ -51,13 +52,18 @@ export default function Post({ post }: PostProps) {
             <h6> <img src="/images/clock.svg" alt="user" /> 4 min</h6>
           </div>
 
-{/*           {
-            Object.entries( post.data.content).map(content => {
-              return(
-                <h2>{content[1]}</h2> 
-              )
-            }) 
-          } */}
+          {post.data.content.map(({ heading, body }) => (
+            <Fragment key={heading}>
+              <h2>{heading}</h2>
+
+              <div
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{
+                  __html: PrismicDOM.RichText.asHtml(body),
+                }}
+              />
+            </Fragment>
+          ))}
 
         </article>
       </main>
@@ -88,9 +94,21 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   const prismic = getPrismicClient({});
   const response = await prismic.getByUID("posts", String(slug));
 
+  const post = {
+    first_publication_date: response.first_publication_date,
+    data: {
+      title: response.data.title,
+      banner: {
+        url: response.data.banner.url,
+      },
+      author: response.data.author,
+      content: response.data.content
+    },
+  };
+
   return {
     props: {
-      response,
+      post,
     },
     revalidate: 60 * 30, // 30 minutes
   }
